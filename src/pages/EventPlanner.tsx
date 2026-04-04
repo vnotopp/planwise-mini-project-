d6086e220cacd7a78416ada4ec3726187de2a726
vnotopp <vedant02@somaiya.edu>
2026-04-04 18:03:47 +0530

    Update EventPlanner.tsx

--- src/pages/EventPlanner.tsx
+++ src/pages/EventPlanner.tsx
@@ -18,7 +18,7 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { toast } from 'sonner';
-import { createParticleBurst } from '@/components/SpaceBackground';
+
 
 const eventSchema = z.object({
   name: z.string().min(1, 'Name required'),
